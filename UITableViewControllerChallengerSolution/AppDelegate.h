0ccf8e3f68c9c6d951ad1022b82e140b76e13077
Jagmeet <thalia@Thalias-iMac.local>
2014-12-14 10:10:24 -0800

    Initial Commit

---
+++ UITableViewControllerChallengerSolution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewControllerChallengerSolution
+//
+//  Created by Jagmeet on 12/14/14.
+//  Copyright (c) 2014 Think Ninja Cut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewControllerChallengerSolution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewControllerChallengerSolution
+//
+//  Created by Jagmeet on 12/14/14.
+//  Copyright (c) 2014 Think Ninja Cut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
